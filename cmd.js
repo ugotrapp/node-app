d1143453bf6fc692cfb4d7f0a73ea29bc4ae5e26
Jblavisse <jean-baptiste@jblavisse.fr>
2021-05-27 13:46:46 +0200

    List notes

--- cmd.js
+++ cmd.js
@@ -6,6 +6,23 @@ yargs.command({
     describe: 'Liste toutes mes notes',
     handler: () => {
         console.log("Voici la liste des notes");
+
+        fs.readFile("data.json", "utf-8", (err,data) => {
+            if(err) console.log(err);
+            else {
+                const notes = JSON.parse(data);
+
+                // Boucle for classique
+                // for(let i=0; i<notes.length;i++) {
+                //     console.log(`${notes[i].id}. ${notes[i].title}`);
+                // }
+
+                // foreach
+                notes.forEach(note => {
+                    console.log(`${note.id}. ${note.title}`);
+                })
+            }
+        })
     }
 }).command({
     command: 'add',
